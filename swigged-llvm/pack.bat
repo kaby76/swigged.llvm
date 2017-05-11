7b1dfd074cb691d4b06052d2dbec85b6de239886
Ken Domino <ken.domino@gmail.com>
2017-05-11 11:54:41 -0400

    Clean up, new build, new packing/dependencies, new release.

--- swigged-llvm/pack.bat
+++ swigged-llvm/pack.bat
@@ -1,1 +1,1 @@
-nuget pack swigged.llvm.nuspec -Version 4.0.4-alpha -Properties 'Configuration=Release;Platform=x86;'
+nuget pack swigged.llvm.nuspec -Version 4.0.5-alpha -Properties 'Configuration=Release;Platform=x86;'
