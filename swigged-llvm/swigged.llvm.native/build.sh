5dfa8490082d57088a31bdd779c1b29199c33bd6
Ken Domino <ken.domino@gmail.com>
2017-05-18 12:29:03 -0400

    Adjustment.

--- swigged-llvm/swigged.llvm.native/build.sh
+++ swigged-llvm/swigged.llvm.native/build.sh
@@ -6,11 +6,11 @@ mkdir ubuntu.16.04-Debug
 mkdir ubuntu.16.04-Release
 
 cd ubuntu.16.04-Debug
-cmake -DCMAKE_BUILD_TYPE=Debug "-DLLVM_DIR=`pwd`/../../llvm/ubuntu.16.04-Debug/lib/cmake/llvm" -DLLVM_TARGETS_TO_BUILD=X86  ..
+cmake -DCMAKE_BUILD_TYPE=Debug -DLLVM_TARGETS_TO_BUILD=X86 -DLLVM_DIR=`pwd`"/../../llvm/ubuntu.16.04-Debug/lib/cmake/llvm"  ..
 cd ..
 
 cd ubuntu.16.04-Release
-cmake -DCMAKE_BUILD_TYPE=Release "-DLLVM_DIR=`pwd`/../../llvm/ubuntu.16.04-Release/lib/cmake/llvm" -DLLVM_TARGETS_TO_BUILD=X86 ..
+cmake -DCMAKE_BUILD_TYPE=Release -DLLVM_TARGETS_TO_BUILD=X86 -DLLVM_DIR=`pwd`"/../../llvm/ubuntu.16.04-Release/lib/cmake/llvm"  ..
 cd ..
 
 cd ubuntu.16.04-Debug
