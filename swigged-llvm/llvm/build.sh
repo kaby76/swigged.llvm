e0353daa689f060ce1923e14838fd6735e52503a
Ken Domino <ken.domino@gmail.com>
2017-05-18 08:22:44 -0400

    Adding build script for Ubuntu.

---
+++ swigged-llvm/llvm/build.sh
@@ -0,0 +1,20 @@
+
+bash -c "rm -rf ubuntu-16.04-Debug"
+bash -c "rm -rf ubuntu-16.04-Release"
+
+mkdir ubuntu-16.04-Debug
+mkdir ubuntu-16.04-Release
+
+cd ubuntu-16.04-Debug
+cmake -DCMAKE_BUILD_TYPE=Debug -DLLVM_TARGETS_TO_BUILD=X86 ..\llvm
+cd ..\ubuntu-16.04-Release
+cmake -DCMAKE_BUILD_TYPE=Release -DLLVM_TARGETS_TO_BUILD=X86 ..\llvm
+
+cd ..
+
+cd ubuntu-16.04-Debug
+make
+cd ..\ubuntu-16.04-Release
+make
+
+cd ..
