7816aec6a123cb1d97209441aeffe11c6906c6b0
Ken Domino <ken.domino@gmail.com>
2017-06-15 12:06:17 -0400

    Added PTX compile and run using ManagedCuda. The MF actually works.

--- swigged-llvm/sanity-test/Program.cs
+++ swigged-llvm/sanity-test/Program.cs
@@ -1,6 +1,10 @@
 ﻿using System;
 using System.Diagnostics;
+using System.IO;
 using System.Runtime.InteropServices;
+using System.Text;
+using ManagedCuda;
+using ManagedCuda.VectorTypes;
 using Swigged.LLVM;
 
 namespace ConsoleApplication1
@@ -246,7 +250,7 @@ namespace ConsoleApplication1
         {
             ModuleRef mod = LLVM.ModuleCreateWithName("llvmptx");
             var pt = LLVM.PointerType(LLVM.Int64Type(), 1);
-            TypeRef[] param_types = { pt };
+            TypeRef[] param_types = {pt};
             TypeRef ret_type = LLVM.FunctionType(LLVM.VoidType(), param_types, false);
             ValueRef sum = LLVM.AddFunction(mod, "sum", ret_type);
             BasicBlockRef entry = LLVM.AppendBasicBlock(sum, "entry");
@@ -270,25 +274,47 @@ namespace ConsoleApplication1
                 CodeGenOptLevel.CodeGenLevelDefault,
                 RelocMode.RelocDefault,
                 CodeModel.CodeModelKernel);
-
+            ContextRef context_ref = LLVM.ContextCreate();
+            ValueRef kernelMd = LLVM.MDNodeInContext(context_ref, new ValueRef[3]
+            {
+                sum,
+                LLVM.MDStringInContext(context_ref, "kernel", 6),
+                LLVM.ConstInt(LLVM.Int32TypeInContext(context_ref), 1, false)
+            });
+            LLVM.AddNamedMetadataOperand(mod, "nvvm.annotations", kernelMd);
             var y1 = LLVM.TargetMachineEmitToMemoryBuffer(
-                    tmr,
-                    mod,
-                    Swigged.LLVM.CodeGenFileType.AssemblyFile,
-                    the_error,
-                    out MemoryBufferRef buffer);
-                    
+                tmr,
+                mod,
+                Swigged.LLVM.CodeGenFileType.AssemblyFile,
+                the_error,
+                out MemoryBufferRef buffer);
+            string ptx = null;
             try
             {
-                string start = LLVM.GetBufferStart(buffer);
+                ptx = LLVM.GetBufferStart(buffer);
                 uint length = LLVM.GetBufferSize(buffer);
-                System.Console.WriteLine(start);
+                // Output the PTX assembly code. We can run this using the CUDA Driver API
+                System.Console.WriteLine(ptx);
             }
             finally
             {
                 LLVM.DisposeMemoryBuffer(buffer);
             }
 
+
+            // RUN THE MF.
+
+            Int64[] h_C = new Int64[100];
+            CudaContext ctx = new CudaContext(CudaContext.GetMaxGflopsDeviceId());
+            CudaKernel kernel = ctx.LoadKernelPTX(Encoding.ASCII.GetBytes(ptx), "sum");
+            var d_C = new CudaDeviceVariable<Int64>(100);
+            int N = 1;
+            int threadsPerBlock = 256;
+            kernel.BlockDimensions = threadsPerBlock;
+            kernel.GridDimensions = (N + threadsPerBlock - 1) / threadsPerBlock;
+            kernel.Run(d_C.DevicePointer);
+            h_C = d_C;
+            System.Console.WriteLine("Result " + h_C[0]);
 
             //OrcJITStackRef ojsr = LLVM.OrcCreateInstance(tmr);
             //MyString ms = new MyString();
