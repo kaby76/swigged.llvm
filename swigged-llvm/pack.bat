36760555d1a3ef6274d978c0e24973f09459b864
Ken Domino <kenneth.domino@domemtech.com>
2017-09-06 07:26:16 -0400

    Fixed .targets file. Release.

--- swigged-llvm/pack.bat
+++ swigged-llvm/pack.bat
@@ -1,1 +1,1 @@
-nuget pack swigged.llvm.nuspec -Version 4.0.12 -Properties 'Configuration=Release;Platform=x86;'
+nuget pack swigged.llvm.nuspec -Version 4.0.13 -Properties 'Configuration=Release;Platform=x86;'
