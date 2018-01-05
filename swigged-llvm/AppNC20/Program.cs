5cd7ddbcee3e43525a90ab019d2cec4f600195d4
Ken Domino <kenneth.domino@domemtech.com>
2018-01-05 12:30:42 -0500

    Fixing targets. Preparing for release.

--- swigged-llvm/AppNC20/Program.cs
+++ swigged-llvm/AppNC20/Program.cs
@@ -2,7 +2,7 @@
 using System.Runtime.InteropServices;
 using Swigged.LLVM;
 
-namespace core.sanity_test
+namespace AppNC20
 {
     class Program
     {
--- swigged-llvm/pack.bat
+++ swigged-llvm/pack.bat
@@ -1,1 +1,1 @@
-nuget pack swigged.llvm.nuspec -Version 5.0.3 -Properties 'Configuration=Release;Platform=x86;'
+nuget pack swigged.llvm.nuspec -Version 5.0.4 -Properties 'Configuration=Release;Platform=x86;'
