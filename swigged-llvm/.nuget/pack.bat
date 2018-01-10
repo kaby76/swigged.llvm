014b063eb0b71d284852b3f72f7ae4a8b7786856
Ken Domino <kenneth.domino@domemtech.com>
2018-01-10 12:08:55 -0500

    Release alpha version.

--- swigged-llvm/.nuget/pack.bat
+++ swigged-llvm/.nuget/pack.bat
@@ -1,1 +1,1 @@
-nuget pack swigged.llvm.nuspec -Version 6.0.0.1 -Properties 'Configuration=Release;Platform=x86;'
+nuget pack swigged.llvm.nuspec -Version 6.0.0.1-alpha -Properties 'Configuration=Release;Platform=x86;'
--- swigged-llvm/swigged.llvm.native/build.ps1
+++ swigged-llvm/swigged.llvm.native/build.ps1
@@ -14,42 +14,11 @@ function Invoke-CmdScript {
   }
 }
 
-
-bash -c "rm -rf x86-Debug"
-bash -c "rm -rf x86-Release"
-bash -c "rm -rf x64-Debug"
 bash -c "rm -rf x64-Release"
 
-mkdir x86-Debug
-mkdir x86-Release
-mkdir x64-Debug
 mkdir x64-Release
-
-cd x86-Debug
-Invoke-CmdScript "%VSINSTALLDIR%\VC\Auxiliary\Build\vcvarsall.bat" x86
-cmake   "-DLLVM_DIR=%CD%\..\..\llvm\x86-Debug\lib\cmake\llvm"    -G "Visual Studio 15 2017" ..
-cd ..\x86-Release
-cmake   "-DLLVM_DIR=%CD%\..\..\llvm\x86-Release\lib\cmake\llvm"    -G "Visual Studio 15 2017" ..
-
 Invoke-CmdScript "%VSINSTALLDIR%\VC\Auxiliary\Build\vcvarsall.bat" x64
-cd ..\x64-Debug
-cmake "-DLLVM_DIR=%CD%\..\..\llvm\x64-Debug\lib\cmake\llvm"    -G "Visual Studio 15 2017 Win64" ..
-cd ..\x64-Release
+cd x64-Release
 cmake "-DLLVM_DIR=%CD%\..\..\llvm\x64-Release\lib\cmake\llvm"    -G "Visual Studio 15 2017 Win64" ..
-
-cd ..
-
-Invoke-CmdScript "%VSINSTALLDIR%\VC\Auxiliary\Build\vcvarsall.bat" x86
-cd x86-Debug
-msbuild swigged-llvm-native.sln /p:Configuration=Debug /p:Platform=Win32
-cd ..\x86-Release
-msbuild swigged-llvm-native.sln /p:Configuration=Release /p:Platform=Win32
-
-
-Invoke-CmdScript "%VSINSTALLDIR%\VC\Auxiliary\Build\vcvarsall.bat" x64
-cd ..\x64-Debug
-msbuild swigged-llvm-native.sln /p:Configuration=Debug /p:Platform=x64
-cd ..\x64-Release
 msbuild swigged-llvm-native.sln /p:Configuration=Release /p:Platform=x64
-
 cd ..
