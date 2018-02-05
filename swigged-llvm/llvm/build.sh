55ebf85585663e0405a706a30b8db6209f392371
Ken Domino <kenneth.domino@domemtech.com>
2018-02-05 09:23:54 -0500

    Updates for release and build.

--- swigged-llvm/llvm/build.sh
+++ swigged-llvm/llvm/build.sh
@@ -1,96 +1,59 @@
 
-export OLDPATH="$PATH"
-export PATH="$OLDPATH:/home/ken/cmake-3.8.1-Linux-x86_64/bin"
-
-echo ""
-echo "Cmake/make of ubuntu-16.04-Debug"
-bash -c "rm -rf ubuntu-16.04-Debug"
-mkdir ubuntu-16.04-Debug
-cd ubuntu-16.04-Debug
-cmake -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON -DCMAKE_BUILD_TYPE=Debug  ../llvm
-make VERBOSE=1
-cd ..
-
-echo ""
 echo "Cmake/make of ubuntu-16.04-Release"
-bash -c "rm -rf ubuntu-16.04-Release"
+rm -rf ubuntu-16.04-Release
+rm -f ubuntu-16.04-Release.tar
+rm -f ubuntu-16.04-Release.tar.gz
 mkdir ubuntu-16.04-Release
 cd ubuntu-16.04-Release
 cmake -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON -DCMAKE_ASM_FLAGS_RELWITHDEBINFO="-O2 -g0 -DNDEBUG" -DCMAKE_CXX_FLAGS_RELWITHDEBINFO="-O2 -g0 -DNDEBUG" -DCMAKE_C_FLAGS_RELWITHDEBINFO="-O2 -g0 -DNDEBUG" -DCMAKE_BUILD_TYPE=Release ../llvm
 make VERBOSE=1
+# If all works, then clean up everything, and create .tar.gz file.
+rm -f Release\bin\BuildingAJIT*
+rm -f Release\bin\Fibonacci
+rm -rf Release\bin\HowToUseJIT
+rm -rf Release\bin\Kaleidoscope-*
+rm -rf Release\bin\llvm-c-test
+rm -rf Release\bin\llvm-lto*
+rm -rf tools
+rm -rf lib\Analysis
+rm -rf lib\AsmParser
+rm -rf lib\BinaryFormat
+rm -rf lib\Bitcode
+rm -rf lib\CodeGen
+rm -rf lib\DebugInfo
+rm -rf lib\Demangle
+rm -rf lib\ExecutionEngine
+rm -rf lib\Fuzzer
+rm -rf lib\IR
+rm -rf lib\IRReader
+rm -rf lib\LineEditor
+rm -rf lib\Linker
+rm -rf lib\LTO
+rm -rf lib\MC
+rm -rf lib\Object
+rm -rf lib\ObjectYAML
+rm -rf lib\Option
+rm -rf lib\Passes
+rm -rf lib\ProfileData
+rm -rf lib\Support
+rm -rf lib\TableGen
+rm -rf lib\Target
+rm -rf lib\Testing
+rm -rf lib\ToolDrivers
+rm -rf lib\Transforms
+rm -rf lib\XRay
+rm -rf examples
+rm -rf Debug
+rm -rf docs
+rm -rf test
+rm -rf unittests
+rm -rf utils
 cd ..
-
+mkdir x64-Release/llvm
+cp -r ./llvm/include ubuntu-16.04-Release/llvm
+pwd
+ls -l ubuntu-16.04-Release
+tar cvf ubuntu-16.04-Release.tar ubuntu-16.04-Release
+gzip -9 ubuntu-16.04-Release.tar
 
 echo "Cannot build LLVM for Android Arm, so passing for now."
-exit 0
-
-
-#####################################################
-#
-# List of all ABIs for Android here:
-# https://developer.android.com/ndk/guides/abis.html
-#
-#####################################################
-
-
-echo ""
-echo "Cmake/make of android-armeabi-Release"
-bash -c "rm -rf android-armeabi-Release"
-mkdir android-armeabi-Release
-# https://developer.android.com/ndk/guides/cmake.html
-# Make sure to install Android Studio, and with that, configure it to
-# include NDK, cmake, ....
-cd  android-armeabi-Release
-export PATH="/home/ken/Android/Sdk/cmake/3.6.3155560/bin:$OLDPATH"
-# ? not version ? 3.6.0-rc2...... cmake ../llvm -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON   -DCMAKE_ANDROID_ARCH_ABI=armeabi   -DANDROID_NDK=/home/ken/Android/Sdk/ndk-bundle -DCMAKE_ASM_FLAGS_RELWITHDEBINFO="-O2 -g0 -DNDEBUG" -DCMAKE_CXX_FLAGS_RELWITHDEBINFO="-O2 -g0 -DNDEBUG" -DCMAKE_C_FLAGS_RELWITHDEBINFO="-O2 -g0 -DNDEBUG" -DCMAKE_CXX_FLAGS_DEBUG=""  -DCMAKE_BUILD_TYPE=Release     -DCMAKE_TOOLCHAIN_FILE=/home/ken/Android/Sdk/ndk-bundle/build/cmake/android.toolchain.cmake   -DANDROID_NATIVE_API_LEVEL=23   -DANDROID_TOOLCHAIN=clang   -DLLVM_TARGETS_TO_BUILD=ARM
-cmake --version
-cmake ../llvm \
-   -DANDROID_NDK=/home/ken/Android/Sdk/ndk-bundle \
-   -DANDROID_NATIVE_API_LEVEL=23 \
-   -DANDROID_TOOLCHAIN=clang  \
-   -DANDROID_ABI=armeabi \
-   -DCMAKE_CXX_FLAGS_RELEASE="-g0" \
-   -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON \
-   -DCMAKE_ANDROID_ARCH_ABI=armeabi \
-   -DCMAKE_ANDROID_ARCH=armeabi \
-   -DCMAKE_ASM_FLAGS_RELWITHDEBINFO="-O2 -g0 -DNDEBUG" \
-   -DCMAKE_CXX_FLAGS_RELWITHDEBINFO="-O2 -g0 -DNDEBUG" \
-   -DCMAKE_C_FLAGS_RELWITHDEBINFO="-O2 -g0 -DNDEBUG" \
-   -DCMAKE_CXX_FLAGS_DEBUG=""  \
-   -DCMAKE_BUILD_TYPE=Release \
-   -DCMAKE_TOOLCHAIN_FILE=/home/ken/Android/Sdk/ndk-bundle/build/cmake/android.toolchain.cmake
-# Note, there is a bug in the make of LLVM. Make everything regardless of errors.
-make VERBOSE=1 -k
-cd ..
-
-
-echo ""
-echo "Cmake/make of android-x86-Release"
-bash -c "rm -rf android-x86-Release"
-mkdir android-x86-Release
-# https://developer.android.com/ndk/guides/cmake.html
-# Make sure to install Android Studio, and with that, configure it to
-# include NDK, cmake, ....
-cd  android-x86-Release
-export PATH="/home/ken/Android/Sdk/cmake/3.6.3155560/bin:$OLDPATH"
-cmake --version
-cmake ../llvm \
-   -DANDROID_NDK=/home/ken/Android/Sdk/ndk-bundle \
-   -DANDROID_NATIVE_API_LEVEL=23 \
-   -DANDROID_TOOLCHAIN=clang  \
-   -DANDROID_ABI=x86 \
-   -DCMAKE_CXX_FLAGS_RELEASE="-g0" \
-   -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON \
-   -DCMAKE_ANDROID_ARCH_ABI=x86 \
-   -DCMAKE_ANDROID_ARCH=x86 \
-   -DCMAKE_ASM_FLAGS_RELWITHDEBINFO="-O2 -g0 -DNDEBUG" \
-   -DCMAKE_CXX_FLAGS_RELWITHDEBINFO="-O2 -g0 -DNDEBUG" \
-   -DCMAKE_C_FLAGS_RELWITHDEBINFO="-O2 -g0 -DNDEBUG" \
-   -DCMAKE_CXX_FLAGS_DEBUG=""  \
-   -DCMAKE_BUILD_TYPE=Release \
-   -DCMAKE_TOOLCHAIN_FILE=/home/ken/Android/Sdk/ndk-bundle/build/cmake/android.toolchain.cmake
-
-# Note, there is a bug in the make of LLVM. Make everything regardless of errors.
-make VERBOSE=1 -k
-cd ..
-
